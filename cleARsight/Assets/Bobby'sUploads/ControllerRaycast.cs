9043e13b46c028db462513e0b67f33590e328471
BXB2 <34118361+BXB2@users.noreply.github.com>
2019-01-18 15:21:31 -0500

    Added Meshing and Raycast
    Added functionality for meshing and raycasting simplified some scripts

---
+++ cleARsight/Assets/Bobby'sUploads/ControllerRaycast.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class ControllerRaycast : MonoBehaviour
+{
+
+    public Transform ctTransform; //Controller transform
+    public GameObject prefab;    // Cube prefab
+
+    // Use this for initialization
+    void Start()
+    {
+        MLWorldRays.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Create a raycast parameters variable
+        MLWorldRays.QueryParams _raycastParams = new MLWorldRays.QueryParams
+        {
+            // Update the parameters with our Camera's transform
+            Position = ctTransform.position,
+            Direction = ctTransform.forward,
+            UpVector = ctTransform.up,
+            // Provide a size of our raycasting array (1x1)
+            Width = 1,
+            Height = 1
+        };
+        // Feed our modified raycast parameters and handler to our raycast request
+        MLWorldRays.GetWorldRays(_raycastParams, HandleOnReceiveRaycast);
+    }
+    private void OnDestroy()
+    {
+        MLWorldRays.Stop();
+    }
+    // Instantiate the prefab at the given point.
+    // Rotate the prefab to match given normal.
+    // Wait 2 seconds then destroy the prefab.
+    private IEnumerator NormalMarker(Vector3 point, Vector3 normal)
+    {
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, normal);
+        GameObject go = Instantiate(prefab, point, rotation);
+        yield return new WaitForSeconds(2);
+        Destroy(go);
+    }
+
+    // Use a callback to know when to run the NormalMaker() coroutine.
+    void HandleOnReceiveRaycast(MLWorldRays.MLWorldRaycastResultState state, UnityEngine.Vector3 point, Vector3 normal, float confidence)
+    {
+        if (state == MLWorldRays.MLWorldRaycastResultState.HitObserved)
+        {
+            StartCoroutine(NormalMarker(point, normal));
+        }
+    }
+
+    // When the prefab is destroyed, stop MLWorldRays API from running.
+}
---
+++ cleARsight/Assets/Bobby'sUploads/cleARsightMeshing.cs
@@ -0,0 +1,192 @@
+﻿// %BANNER_BEGIN%
+// ---------------------------------------------------------------------
+// %COPYRIGHT_BEGIN%
+//
+// Copyright (c) 2018 Magic Leap, Inc. All Rights Reserved.
+// Use of this file is governed by the Creator Agreement, located
+// here: https://id.magicleap.com/creator-terms
+//
+// %COPYRIGHT_END%
+// ---------------------------------------------------------------------
+// %BANNER_END%
+
+#if UNITY_EDITOR || PLATFORM_LUMIN
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.MagicLeap;
+
+namespace MagicLeap
+{
+    /// <summary>
+    /// This represents all the runtime control over meshing component in order to best visualize the
+    /// affect changing parameters has over the meshing API.
+    /// </summary>
+    public class cleARsightMeshing : MonoBehaviour
+    {
+        #region Private Variables
+        [SerializeField, Tooltip("The spatial mapper from which to update mesh params.")]
+        private MLSpatialMapper _mlSpatialMapper;
+
+        [SerializeField, Tooltip("Visualizer for the meshing results.")]
+        private MeshingVisualizer _meshingVisualizer;
+
+
+        [SerializeField, Space, Tooltip("Flag specifying if mesh extents are bounded.")]
+        private bool _bounded = false;
+
+
+        [SerializeField, Space, Tooltip("ControllerConnectionHandler reference.")]
+        private ControllerConnectionHandler _controllerConnectionHandler;
+
+        private MeshingVisualizer.RenderMode _renderMode = MeshingVisualizer.RenderMode.Wireframe;
+        private int _renderModeCount;
+
+        private static readonly Vector3 _boundedExtentsSize = new Vector3(2.0f, 2.0f, 2.0f);
+        private static readonly Vector3 _boundlessExtentsSize = new Vector3(10.0f, 10.0f, 10.0f);
+
+
+        private Camera _camera;
+        #endregion
+
+        #region Unity Methods
+        /// <summary>
+        /// Initializes component data and starts MLInput.
+        /// </summary>
+        void Awake()
+        {
+            if (_mlSpatialMapper == null)
+            {
+                Debug.LogError("Error: MeshingExample._mlSpatialMapper is not set, disabling script.");
+                enabled = false;
+                return;
+            }
+            if (_meshingVisualizer == null)
+            {
+                Debug.LogError("Error: MeshingExample._meshingVisualizer is not set, disabling script.");
+                enabled = false;
+                return;
+            }
+            if (_controllerConnectionHandler == null)
+            {
+                Debug.LogError("Error MeshingExample._controllerConnectionHandler not set, disabling script.");
+                enabled = false;
+                return;
+            }
+
+            _renderModeCount = System.Enum.GetNames(typeof(MeshingVisualizer.RenderMode)).Length;
+
+            _camera = Camera.main;
+
+            MLInput.OnControllerButtonDown += OnButtonDown;
+            MLInput.OnTriggerDown += OnTriggerDown;
+            MLInput.OnControllerTouchpadGestureStart += OnTouchpadGestureStart;
+            MagicLeapDevice.RegisterOnHeadTrackingMapEvent(OnHeadTrackingMapEvent);
+        }
+
+        /// <summary>
+        /// Set correct render mode for meshing and update meshing settings.
+        /// </summary>
+        void Start()
+        {
+            _meshingVisualizer.SetRenderers(_renderMode);
+
+            _mlSpatialMapper.gameObject.transform.position = _camera.gameObject.transform.position;
+            _mlSpatialMapper.gameObject.transform.localScale = _bounded ? _boundedExtentsSize : _boundlessExtentsSize;
+            
+        }
+
+        /// <summary>
+        /// Update mesh polling center position to camera.
+        /// </summary>
+        void Update()
+        {
+            _mlSpatialMapper.gameObject.transform.position = _camera.gameObject.transform.position;
+        }
+
+        /// <summary>
+        /// Cleans up the component.
+        /// </summary>
+        void OnDestroy()
+        {
+            MagicLeapDevice.UnregisterOnHeadTrackingMapEvent(OnHeadTrackingMapEvent);
+            MLInput.OnControllerTouchpadGestureStart -= OnTouchpadGestureStart;
+            MLInput.OnTriggerDown -= OnTriggerDown;
+            MLInput.OnControllerButtonDown -= OnButtonDown;
+        }
+        #endregion
+        
+
+        #region Event Handlers
+        /// <summary>
+        /// Handles the event for button down. Changes render mode if bumper is pressed or
+        /// changes from bounded to boundless and viceversa if home button is pressed.
+        /// </summary>
+        /// <param name="controllerId">The id of the controller.</param>
+        /// <param name="button">The button that is being released.</param>
+        private void OnButtonDown(byte controllerId, MLInputControllerButton button)
+        {
+            if (_controllerConnectionHandler.IsControllerValid(controllerId))
+            {
+                if (button == MLInputControllerButton.Bumper)
+                {
+                    _renderMode = (MeshingVisualizer.RenderMode)((int)(_renderMode + 1) % _renderModeCount);
+                    _meshingVisualizer.SetRenderers(_renderMode);
+                }
+                else if (button == MLInputControllerButton.HomeTap)
+                {
+                    _bounded = !_bounded;
+                    
+                    _mlSpatialMapper.gameObject.transform.localScale = _bounded ? _boundedExtentsSize : _boundlessExtentsSize;
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Handles the event for trigger down. Throws a ball in the direction of
+        /// the camera's forward vector.
+        /// </summary>
+        /// <param name="controllerId">The id of the controller.</param>
+        /// <param name="button">The button that is being released.</param>
+        private void OnTriggerDown(byte controllerId, float value)
+        {
+            if (_controllerConnectionHandler.IsControllerValid(controllerId))
+            {
+               
+            }
+        }
+
+        /// <summary>
+        /// Handles the event for touchpad gesture start. Changes level of detail
+        /// if gesture is swipe up.
+        /// </summary>
+        /// <param name="controllerId">The id of the controller.</param>
+        /// <param name="gesture">The gesture getting started.</param>
+        private void OnTouchpadGestureStart(byte controllerId, MLInputControllerTouchpadGesture gesture)
+        {
+            if (_controllerConnectionHandler.IsControllerValid(controllerId) &&
+                gesture.Type == MLInputControllerTouchpadGestureType.Swipe && gesture.Direction == MLInputControllerTouchpadGestureDirection.Up)
+            {
+                _mlSpatialMapper.levelOfDetail = ((_mlSpatialMapper.levelOfDetail == MLSpatialMapper.LevelOfDetail.Maximum) ? MLSpatialMapper.LevelOfDetail.Minimum : (_mlSpatialMapper.levelOfDetail + 1));
+            }
+        }
+
+        /// <summary>
+        /// Handle in charge of refreshing all meshes if map gets lost.
+        /// </summary>
+        /// <param name="mapEvents"> Map Events that happened. </param>
+        private void OnHeadTrackingMapEvent(MLHeadTrackingMapEvent mapEvents)
+        {
+            if (mapEvents.IsLost())
+            {
+                _mlSpatialMapper.DestroyAllMeshes();
+                _mlSpatialMapper.RefreshAllMeshes();
+            }
+        }
+        #endregion
+    }
+}
+
+#endif
+
